c2d04f9703b49e7444b52cf7fcc3dc16cb6a2fa4
gpmarchi <gustavomarchi@gmail.com>
2023-02-13 11:43:39 -0300

    feat: add eslint and format code

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/hello', () => {{
-	return 'hello world!'
-}})
-
-app.listen({
- port: 3333,
-}).then(() => {
-	console.log('HTTP server running!')
+app.get('/hello', () => {
+  return 'hello world!'
 })
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
