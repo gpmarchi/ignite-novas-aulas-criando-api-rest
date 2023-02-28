f177f8531a72643824fb0c8a6f0ba05b97cc9f05
gpmarchi <gustavomarchi@gmail.com>
2023-02-28 13:36:48 -0300

    test: install vitest and create first example test

---
+++ src/tests/example.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+
+test('o usuário consegue criar uma nova transação', () => {
+  // fazer a chamada HTTP para criar uma nova transação
+
+  const responseStatusCode = 201
+
+  // validação
+  expect(responseStatusCode).toEqual(201)
+})
