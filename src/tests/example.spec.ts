01a17aaf4dc4a7a5d19803808e467129a53da14c
gpmarchi <gustavomarchi@gmail.com>
2023-03-01 12:19:50 -0300

    test: add first integration test case

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -1,10 +1,21 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, expect, test } from 'vitest'
+import { app } from '../app'
 
-test('o usuário consegue criar uma nova transação', () => {
-  // fazer a chamada HTTP para criar uma nova transação
+beforeAll(async () => {
+  await app.ready()
+})
 
-  const responseStatusCode = 201
+afterAll(async () => {
+  await app.close()
+})
 
-  // validação
-  expect(responseStatusCode).toEqual(201)
+test('user can create a new transaction', async () => {
+  const response = await request(app.server).post('/transactions').send({
+    title: 'New transaction',
+    amount: 5000,
+    type: 'credit',
+  })
+
+  expect(response.statusCode).toEqual(201)
 })
