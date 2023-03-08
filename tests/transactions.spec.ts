ce30966f84ca0a2d9097c10675bc10ee78e52a1d
gpmarchi <gustavomarchi@gmail.com>
2023-03-08 17:48:54 -0300

    refactor: move tests to project root folder

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
-import { app } from '../app'
+import { app } from '../src/app'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
